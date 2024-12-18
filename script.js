1c358e3c7df8d6b9e79bfba154a400b871387e1a
ericafk0001 <dxslyofficial@gmail.com>
2024-12-17 21:19:47 -0500

    boba fluid, boba plastic cup, boba spout, matterjs renderer using a carton template :D

--- script.js
+++ script.js
@@ -1,32 +1,175 @@
-// module aliases
-var Engine = Matter.Engine,
-  Render = Matter.Render,
-  Runner = Matter.Runner,
-  Bodies = Matter.Bodies,
-  Composite = Matter.Composite;
+const canvas = document.querySelector("#matter-canvas");
+let canvasWidth = innerWidth;
+let canvasHeight = innerHeight;
 
-// create an engine
-var engine = Engine.create();
+const stdDeviation = [8, 10];
+const colorMatrix = ["15 -3", "30 -5"];
 
-// create a renderer
-var render = Render.create({
-  element: document.body,
-  engine: engine,
+window.addEventListener("DOMContentLoaded", () => {
+  const {
+    Engine,
+    Render,
+    Runner,
+    Bodies,
+    Composite,
+    Mouse,
+    MouseConstraint,
+    Events,
+    Body,
+  } = Matter;
+
+  let engine, render, runner, mouse, mouseConstraint;
+
+  let circles = [];
+  let glass;
+
+  function init() {
+    engine = Engine.create({
+      constraintIterations: 10,
+      positionIterations: 10,
+    });
+
+    render = Render.create({
+      canvas: canvas,
+      engine: engine,
+      options: {
+        width: canvasWidth,
+        height: canvasHeight,
+        wireframes: false,
+        background: "transparent",
+        pixelRatio: 1,
+      },
+    });
+
+    mouse = Mouse.create(canvas);
+
+    mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.1,
+        render: { visible: false },
+      },
+    });
+
+    runner = Runner.create();
+
+    Render.run(render);
+    Runner.run(runner, engine);
+
+    mouse.element.removeEventListener("mousewheel", mouse.mousewheel);
+    mouse.element.removeEventListener("DOMMouseScroll", mouse.mousewheel);
+    Composite.add(engine.world, mouseConstraint);
+  }
+
+  function createLiquid() {
+    const x = 105;
+    const y = 105;
+    const radius = randomNumBetween(6, 7);
+    const body = Bodies.circle(x, y, radius, {
+      friction: 0,
+      density: 1,
+      frictionAir: 0,
+      restitution: 0.7,
+      render: { fillStyle: "#edcfaf" },
+    });
+    Body.applyForce(body, body.position, { x: 1, y: 0 });
+    Composite.add(engine.world, body);
+    circles.push(body);
+  }
+
+  function BobaCup() {
+    const bobaImg = document.querySelector("#boba-cup");
+    this.cx = canvasWidth * 0.5;
+    this.cy = canvasHeight * 0.8;
+    const thickness = 20;
+    const wallColor = "#000000000";
+
+    const left = Bodies.rectangle(this.cx - 70, this.cy, thickness, 230, {
+      chamfer: { radius: 10 },
+      isStatic: true,
+      angle: (Math.PI / 180) * -8,
+      render: { fillStyle: wallColor },
+    });
+    const right = Bodies.rectangle(this.cx + 70, this.cy, thickness, 230, {
+      chamfer: { radius: 10 },
+      isStatic: true,
+      angle: (Math.PI / 180) * 8,
+      render: { fillStyle: wallColor },
+    });
+    const bottom = Bodies.rectangle(
+      this.cx,
+      this.cy + 125,
+      150,
+      thickness * 2,
+      {
+        chamfer: { radius: 0 },
+        isStatic: true,
+        render: { fillStyle: wallColor },
+      }
+    );
+    bobaImg.style.transform = `translate(${this.cx - canvasWidth / 2}px, ${
+      this.cy - canvasHeight / 2
+    }px)`;
+
+    Composite.add(engine.world, [left, right, bottom]);
+
+    this.setPosition = (pos) => {
+      this.cx = pos.x;
+      this.cy = pos.y;
+      Body.setPosition(left, { x: this.cx - 70, y: this.cy });
+      Body.setPosition(right, { x: this.cx + 70, y: this.cy });
+      Body.setPosition(bottom, { x: this.cx, y: this.cy + 125 });
+      bobaImg.style.transform = `translate(${this.cx - canvasWidth / 2}px, ${
+        this.cy - canvasHeight / 2
+      }px)`;
+    };
+  }
+
+  init();
+  resizeFilter();
+  glass = new BobaCup();
+
+  Events.on(mouseConstraint, "mousemove", (e) => {
+    glass.setPosition({
+      x: e.mouse.position.x,
+      y: e.mouse.position.y,
+    });
+  });
+
+  Events.on(runner, "tick", (e) => {
+    createLiquid();
+    for (let i = circles.length - 1; i >= 0; i--) {
+      if (circles[i].position.y - circles[i].circleRadius > canvasHeight) {
+        Composite.remove(engine.world, circles[i]);
+        circles.splice(i, 1);
+      }
+    }
+  });
+
+  function resizeFilter() {
+    const feGaussianBlur = document.querySelector("#gooey feGaussianBlur");
+    const feColorMatrix = document.querySelector("#gooey feColorMatrix");
+    let index;
+    if (canvasWidth < 600) index = 0;
+    else index = 1;
+    feGaussianBlur.setAttribute("stdDeviation", stdDeviation[index]);
+    feColorMatrix.setAttribute(
+      "values",
+      `1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 ${colorMatrix[index]}`
+    );
+  }
+
+  window.addEventListener("resize", () => {
+    canvasWidth = innerWidth;
+    canvasHeight = innerHeight;
+    render.canvas.width = canvasWidth;
+    render.canvas.height = canvasHeight;
+    resizeFilter();
+
+    glass.setPosition({ x: canvasWidth * 0.5, y: canvasHeight * 0.8 });
+  });
 });
 
-var boba = Bodies.circle(400, 100, 20);
-var boba2 = Bodies.circle(400, 100, 20);
-var boba3 = Bodies.circle(370, 100, 20);
-var ground = Bodies.rectangle(400, 450, 120, 15, { isStatic: true });
-
-// add all of the bodies to the world
-Composite.add(engine.world, [boba, boba2, boba3, ground]);
-
-// run the renderer
-Render.run(render);
-
-// create runner
-var runner = Runner.create();
-
-// run the engine
-Runner.run(runner, engine);
+function randomNumBetween(min, max) {
+  return Math.random() * (max - min) + min;
+}
